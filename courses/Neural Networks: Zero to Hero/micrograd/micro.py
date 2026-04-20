c4c17ba2af936999c9d079e8c4080e6e7a24f114
David Gomez-Landero Lopez <d.gomezlanderolopez@gmail.com>
2026-04-20 20:48:33 +0200

    plotting with graphviz

--- courses/Neural Networks: Zero to Hero/micrograd/micro.py
+++ courses/Neural Networks: Zero to Hero/micrograd/micro.py
@@ -1,24 +1,62 @@
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+from graphviz import Digraph
 
 class Value:
-    def __init__(self, data):
+    def __init__(self, data, _children=(), _op=''):
         self.data = data
+        self._prev=set(_children)
+        self._op = _op
+        
     
     def __repr__(self):
         return f"Value(data={self.data})"
     
     def __add__(self, other):
-        out = Value(self.data + other.data)
+        out = Value(self.data + other.data, (self,other), '+')
         return out
     
     def __mul__(self, other):
-        out = Value(self.data * other.data)
+        out = Value(self.data * other.data, (self, other), '*')
         return out
 
+def trace(root):
+    nodes, edges = set(), set()
+    def build(v):
+        if v not in nodes:
+            nodes.add(v)
+            for child in v._prev:
+                edges.add((child, v))
+                build(child)
+    build(root)
+    return nodes, edges
+
+def draw_dot(root):
+    dot = Digraph(format="svg", graph_attr={'rankdir': 'LR'})
+    
+    nodes, edges = trace(root)
+    for n in nodes:
+        uid = str(id(n))
+        dot.node(name=uid, label="{data %.4f}" % (n.data, ), shape='record')
+        if n._op:
+            dot.node(name=uid + n._op, label = n._op)
+            dot.edge(uid + n._op, uid)
+    
+    for n1, n2 in edges:
+        dot.edge(str(id(n1)), str(id(n2)) + n2._op)
+        
+    return dot
+    
 if __name__ == "__main__":
     a = Value(2.0)
     b = Value(-3.0)
+    c = Value(10.0)
     print(a+b)
     print(a*b)
+    d = a * b + c
+    print(d._prev)
+    print(d._op)
+    dot = draw_dot(d)
+    out = dot.render(filename="graph", format="svg", cleanup=True)
+print("Saved to:", out)
